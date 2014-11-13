8554ad279db7751655fc914d6c4b2c881f362e7c
Ashwin <ashwin.raman87@gmail.com>
2014-11-12 21:48:29 -0500

    updated db

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  def index 
+    @content_first = "This is sample text for the blog";
+    @content_second = "This is also sample text for the blog";
+  end  
+  def edit
+    
+  end  
+  def create
+    
+  end
+  def new
+    
+  end
+  def update
+    
+  end
+  def show
+    
+  end
+  def destroy
+    
+  end
+end
